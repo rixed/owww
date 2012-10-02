3f983e8340b0d1fbc1de0570247a1568b41a6afb
Cedric Cellier <rixed@happyleptic.org>
2012-10-02 14:06:36 +0200

    New model for user values.

--- input.ml
+++ input.ml
@@ -6,11 +6,12 @@ open Html
 (** The types and utilities your are likely to want to open *)
 module Ops = struct
 
-    (** The type values from user, that can be set or unset
-     * (for some reason explained by error msg in string) *)
-    type 'a user_value = Unset | Error of (string * string) | Value of 'a
+    (** The type values from user, that can be set or erroneous
+     * (in which case we keep both an error message and the original input
+     * so the user is given a chance to fix it) *)
+    type 'a user_value = Error of (string * string) | Value of 'a
 
-    (** The type of a shareable datatype *)
+    (** The type of an optional user input *)
     module type TYPE =
     sig
         (** The internal type for these values *)
@@ -20,37 +21,54 @@ module Ops = struct
         val to_html : t user_value -> html
         val edit : string -> t user_value -> html
         val from_args : string -> (string, string) Hashtbl.t -> t user_value
-        exception BadInput of (string * string)
-        exception Unset
-        val from_args_exc : string -> (string, string) Hashtbl.t -> t
+    end
+
+    (** The type of an aggregate of user inputs, the difference being that
+     * there are no erroneous nor unset values *)
+    module type AGGR_TYPE =
+    sig
+        (** The internal type for these values *)
+        type t
+
+        val name : string
+        val to_html : t -> html
+        val edit : string -> t -> html
+        val from_args : string -> (string, string) Hashtbl.t -> t
     end
 
     let html_of_user_value f = function
-        | Unset            -> "<i>unset</i>"
         | Error (err, inp) -> inp ^ "&nbsp;<i>(error: "^err^")</i>"
-        | Value x          -> f x
+        | Value x   -> f x
 
     let err_msg_of = function
-        | Value _ | Unset -> []
+        | Value _ -> []
         | Error (err, _inp) ->
             [ p ~attrs:["class","edit-err"]
                 [ cdata err ] ]
 
     let input_of_user_value f = function
-        | Unset -> ""
         | Error (_err, inp) -> inp
         | Value x -> f x
 end
 
 open Ops
 
-(** Implementation for a bounded range integer *)
-module Integer (Limit : sig val min : int val max : int end) :
-    TYPE with type t = int =
+let missing_field = Error ("this field is mandatory", "")
+
+module type LIMIT =
+sig
+    val min : int
+    val max : int
+end
+
+(** Implementation for an optional, bounded range integer *)
+module OptInteger (Limit : LIMIT) :
+    TYPE with type t = int option =
 struct
-    type t = int
-    let name = "integer"
-    let to_html v = [ cdata (html_of_user_value string_of_int v) ]
+    type t = int option
+    let name = "optional integer"
+    let to_html v = [ cdata (html_of_user_value (function None -> "<i>unset</i>"
+                                                        | Some i -> string_of_int i) v) ]
     let edit name v =
         let max_size = float_of_int Limit.max |>
                        log10 |> ceil |>
@@ -58,22 +76,42 @@ struct
         [ input [ "maxlength", string_of_int max_size ;
                   "size", string_of_int max_size ;
                   "name", name ;
-                  "value", input_of_user_value string_of_int v ] ] @
+                  "value", input_of_user_value (function None -> ""
+                                                       | Some i -> string_of_int i) v ] ] @
         err_msg_of v
     let from_args name args =
         match Hashtbl.find_option args name with
-        | None -> Unset 
+        | None -> Value None
         | Some s -> try let n = int_of_string s in
                         if n < Limit.min then (
                             Error (Printf.sprintf "must not be less than %d" Limit.min, s)
                         ) else if n > Limit.max then (
                             Error (Printf.sprintf "must not be greater than %d" Limit.max, s)
-                        ) else Value n
+                        ) else Value (Some n)
                     with Failure _ ->
                         Error ("not an integer", s)
 end
 
-module String :
+(* Same, but the integer is now mandatory *)
+module Integer (Limit : LIMIT) :
+    TYPE with type t = int =
+struct
+    module O = OptInteger (Limit)
+    type t = int
+    let name = "integer"
+    let make_opt = function
+        | Error _ as x -> x
+        | Value n -> Value (Some n)
+    let to_html v = O.to_html (make_opt v)
+    let edit name v = O.edit name (make_opt v)
+    let from_args name args =
+        match O.from_args name args with
+        | Error _ as e -> e
+        | Value None -> missing_field
+        | Value (Some n) -> Value n
+end
+
+module String (Limit : LIMIT) :
     TYPE with type t = string =
 struct
     type t = string
@@ -81,20 +119,25 @@ struct
     let to_html v = [ cdata (html_of_user_value identity v) ]
     let edit name v =
         [ input [ "name", name ;
+                  "size", string_of_int (min 20 Limit.max) ;
                   "value", input_of_user_value identity v ] ] @
         err_msg_of v
     let from_args name args =
-        match Hashtbl.find_option args name with
-        | None -> Unset
-        | Some s -> Value s
+        let s = match Hashtbl.find_option args name with
+                | None -> ""
+                | Some s -> s in
+        let len = String.length s in
+        if len < Limit.min then Error (Printf.sprintf "Input must be at least %d chars long" Limit.min, s)
+        else if len > Limit.max then Error (Printf.sprintf "Input must not exceed %d chars" Limit.max, s)
+        else Value s
 end
 
-module Password :
+module Password (Limit : LIMIT) :
     TYPE with type t = string =
 struct
-    include String
-    let to_html v = [ cdata (match v with Unset | Error _ -> ""
-                                        | Value _ -> "*****") ]
+    include String (Limit)
+    let to_html v = [ cdata (match v with Error _ -> ""
+                                        | _ -> "*****") ]
     let edit name v =
         [ input [ "name", name ;
                   "value", input_of_user_value identity v ;
@@ -102,7 +145,12 @@ struct
         err_msg_of v
 end
 
-module Enum (E : sig val name : string val options : string array end) :
+module type ENUM_OPTIONS =
+sig
+    val name : string val options : string array
+end
+
+module Enum (E : ENUM_OPTIONS) :
     TYPE with type t = int =
 struct
     type t = int
@@ -122,7 +170,7 @@ struct
         err_msg_of v
     let from_args name args =
         match Hashtbl.find_option args name with
-        | None -> Unset
+        | None -> missing_field
         | Some s -> try Value (Array.findi ((=) s) E.options)
                     with Not_found -> Error ("not a valid choice", s)
 end
@@ -147,39 +195,37 @@ end
 
 (** We can cons types together to form tuples, or fields to form records.
  * Notice that the corresponding internal type is not merely the product of both
- * types, since we want to be able to have some unset fields in an otherwise
- * valid record (ie. record is allowed to be half initialized and still be set!) *)
-module ConsOf (T1 : TYPE) (T2 : TYPE) :
-    TYPE with type t = T1.t user_value * T2.t user_value =
+ * types, since we want to be able to have some unset fields in an overall
+ * record that's always valid. *)
+module ConsOf (T1 : TYPE) (T2 : AGGR_TYPE) :
+    AGGR_TYPE with type t = T1.t user_value * T2.t =
 struct
-    type t = T1.t user_value * T2.t user_value
+    type t = T1.t user_value * T2.t
     let name = T1.name^" * "^T2.name
-    let values_of_value = function
-        (* notice: errors are reported on the first field only *)
-        | Unset -> Unset, Unset
-        | Error x -> Error x, Unset
-        | Value (u1, u2) -> u1, u2
-    let value_of_values = function
-        (* and the other way round *)
-        | Unset, Unset -> Unset
-        | Error x, Unset -> Error x
-        | u1, u2 -> Value (u1, u2)
-    let to_html value =
-        let v1, v2 = values_of_value value in
+    let to_html (v1, v2) =
         T1.to_html v1 @
         T2.to_html v2
-    let edit name value =
-        let v1, v2 = values_of_value value in
+    let edit name (v1, v2) =
         T1.edit name v1 @
         T2.edit name v2
     let from_args name args =
-        let v1 = T1.from_args name args
-        and v2 = T2.from_args name args in
-        value_of_values (v1, v2)
+        T1.from_args name args,
+        T2.from_args name args
 end
 
-module RecordOf (T : TYPE) :
-    TYPE with type t = T.t =
+(** Useful as end of ConsOf list *)
+module NulType :
+    AGGR_TYPE with type t = unit =
+struct
+    type t = unit
+    let name = ""
+    let to_html () = []
+    let edit _name () = []
+    let from_args _name _args = ()
+end
+
+module RecordOf (T : AGGR_TYPE) :
+    AGGR_TYPE with type t = T.t =
 struct
     type t = T.t
     let name = "{ "^T.name^" }"
